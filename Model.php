a095f229d9ff71be1b118336e841633315654829
Greyven <greyven@gmail.com>
2018-05-02 15:17:32 +0200

    Factorisation de la connexion BDD
    Création de getDB() qui retourne un objet PDO de connexion.

--- Model.php
+++ Model.php
@@ -1,10 +1,18 @@
 <?php
 
-//Renvoie la liste de tous les posts triés par id decroissant
+// Renvoie la liste de tous les posts triés par id decroissant
 function getPosts()
 {
-	$db = new PDO('mysql:host=localhost;dbname=_myblog;charset=utf8', 'root', '',
-			   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$db = getDb();
 	$posts = $db->query('SELECT post_id AS id, post_date AS date, post_title AS title, post_content AS content FROM posts ORDER BY post_id DESC');
 	return $posts;
+}
+
+// Effectue la connexion à la BDD
+// Instancie et renvoie l'objet PDO associé
+function getDb()
+{
+	$db = new PDO('mysql:host=localhost;dbname=_myblog;charset=utf8', 'root', '',
+				   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	return $db;
 }
