1930451bcf4fdc48588737d361b9d98b0c50831c
Greyven <greyven@gmail.com>
2018-05-20 19:56:54 +0200

    Création menu admin

--- View/Admin/index.php
+++ View/Admin/index.php
@@ -4,10 +4,22 @@
 		$this->title = 'Mon blog - Administration';
 		?>
 
+		<ul class="horizontal">
+			<li><a href="home" class="active">Accueil</a></li>
+			<li><a href="#">Créer billet</a></li>
+			<li><a href="#">Modifier billet</a></li>
+			<li><a href="#">Gérer commentaires</a></li>
+		</ul>
+
+		<br/>
+
 		<h2>Administration</h2>
 
 		Bienvenu Jean!
 		Ce blog comporte <?= $this->clean($postsNb); ?> billet<?php if($this->clean($postsNb) > 1) echo 's'; ?> et <?= $this->clean($commentsNb); ?> commentaire<?php if($this->clean($commentsNb) > 1) echo 's'; ?>.
+		
+		<br/>
+		<br/>		
 
 		<?php
 	}
