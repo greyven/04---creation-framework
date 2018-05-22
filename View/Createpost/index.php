f3c6d2ec0147675b9a632029d83d752daad679eb
Greyven <greyven@gmail.com>
2018-05-22 10:49:40 +0200

    Intégration tinyMCE, premier éssai

--- View/Createpost/index.php
+++ View/Createpost/index.php
@@ -6,11 +6,26 @@
 		require_once('View/menuAdmin.php');
 		?>
 		
-		<div class="container admin shadow">
+		<div class="container admin shadow marginBottom10">
 			<div class="row">
 				<div class="col-lg-12">
 					<h2 class="admin marginLeft10">Création de billet</h2>
 				</div>	
+			</div>
+		</div>
+
+		<div class="container admin shadow">
+			<div class="row">
+				<div class="col-lg-12">
+					<form name="createPost" id="createPost" action="index.html" method="post">
+						<div class="row">
+							<div class="col-lg-12">
+								<textarea id="createPost" name="createPost" rows="25"></textarea>	
+							</div>
+						</div>
+						<button type="submit">Valider</button>
+					</form>
+				</div>
 			</div>
 		</div>
 		
